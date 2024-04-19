511db43c40a6aa26c9eb948421ce49e53e9836ef
tyvekzhang <tyvekzhang@gmail.com>
2024-04-19 23:12:20 +0800

    cleanup: optimize common part-3

--- fss/common/persistence/base_mapper.py
+++ fss/common/persistence/base_mapper.py
@@ -1,79 +1,62 @@
 """BaseMapper defines the implemented functionalities"""
 
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from typing import Any, List
 
-from fss.common.persistence.mapper import Mapper
 
-
-class BaseMapper(Mapper):
+class BaseMapper(ABC):
     @abstractmethod
-    async def insert(self, *, data: Any, db_session: Any) -> int:
-        raise NotImplementedError
+    async def insert(self, *, data: Any, db_session: Any) -> Any: ...
 
     @abstractmethod
-    async def insert_batch(self, *, data_list: List[Any], db_session: Any) -> int:
-        raise NotImplementedError
+    async def insert_batch(self, *, data_list: List[Any], db_session: Any) -> int: ...
 
     @abstractmethod
-    async def select_by_id(self, *, id: Any, db_session: Any) -> Any:
-        raise NotImplementedError
+    async def select_by_id(self, *, id: Any, db_session: Any) -> Any: ...
 
     @abstractmethod
-    async def select_count(self, *, db_session: Any) -> int:
-        raise NotImplementedError
+    async def select_count(self, *, db_session: Any) -> int: ...
 
     @abstractmethod
     async def select_list(
-        self, *, page: int, size: int, query: Any, db_session: Any
-    ) -> List[Any]:
-        raise NotImplementedError
+            self, *, page: int, size: int, db_session: Any, **kwargs
+    ) -> List[Any]: ...
 
     @abstractmethod
     async def select_list_ordered(
-        self,
-        *,
-        page: int,
-        size: int,
-        query: Any,
-        order_by: Any,
-        sort_order: Any,
-        db_session: Any,
-    ) -> List[Any]:
-        raise NotImplementedError
+            self,
+            *,
+            page: int,
+            size: int,
+            order_by: Any,
+            sort_order: Any,
+            db_session: Any,
+            **kwargs
+    ) -> List[Any]: ...
 
     @abstractmethod
-    async def select_list_page(
-        self, *, params: Any, query: Any, db_session: Any
-    ) -> List[Any]:
-        raise NotImplementedError
+    async def select_page(
+            self, *, params: Any, db_session: Any
+    ) -> List[Any]: ...
 
     @abstractmethod
-    async def select_list_page_ordered(
-        self,
-        *,
-        params: Any,
-        query: Any,
-        order_by: Any,
-        sort_order: Any,
-        db_session: Any,
-    ) -> List[Any]:
-        raise NotImplementedError
+    async def select_page_ordered(
+            self,
+            *,
+            params: Any,
+            order_by: Any,
+            sort_order: Any,
+            db_session: Any,
+    ) -> List[Any]: ...
 
     @abstractmethod
-    async def update_by_id(self, *, data: Any, db_session: Any) -> int:
-        raise NotImplementedError
+    async def update_by_id(self, *, data: Any, db_session: Any) -> int: ...
 
     @abstractmethod
-    async def update_batch_by_ids(
-        self, *, data_list: List[Any], db_session: Any
-    ) -> int:
-        raise NotImplementedError
+    async def update_batch_by_ids(self, *, ids: List[Any], data: dict, db_session: Any = None) -> int: ...
 
     @abstractmethod
-    async def delete_by_id(self, *, id: Any, db_session: Any) -> int:
-        raise NotImplementedError
+    async def delete_by_id(self, *, id: Any, db_session: Any) -> int: ...
 
     @abstractmethod
-    async def delete_batch_by_ids(self, *, ids: List[Any], db_session: Any) -> int:
-        raise NotImplementedError
+    async def delete_batch_by_ids(self, *, ids: List[Any], db_session: Any) -> int: ...
