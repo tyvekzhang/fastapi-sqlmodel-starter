31cebea76c9105f81ac30f7619440e0e3ba99929
tyvekzhang <tyvekzhang@gmail.com>
2024-06-06 17:08:41 +0800

    cleanup: optimize empty service display

--- fss/starter/system/service/user_role_service.py
+++ fss/starter/system/service/user_role_service.py
@@ -6,5 +6,4 @@ from fss.common.service.service import Service
 from fss.starter.system.model.user_role_do import UserRoleDO
 
 
-class UserRoleService(Service[UserRoleDO], ABC):
-    pass
+class UserRoleService(Service[UserRoleDO], ABC): ...
